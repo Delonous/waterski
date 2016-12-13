49a352caca32b6089f68838347e23f27fd24f802
Dean Simonelli <simonel3@msu.edu>
2016-12-12 22:50:26 -0500

    updates

---
+++ main.js
@@ -0,0 +1,15 @@
+function size() {
+    if (window.innerWidth < 768) {
+        document.getElementById('spartyhead1').style.display = 'none';
+        document.getElementById('spartyhead2').style.display = 'none';
+        document.getElementById('twit2').style.display = 'none';
+        document.getElementById('insta2').style.display = 'none';
+        document.getElementById('fb2').style.display = 'none';
+    } else {
+        document.getElementById('spartyhead1').style.display = 'inline';
+        document.getElementById('spartyhead2').style.display = 'inline';
+        document.getElementById('twit1').style.display = 'inline';
+        document.getElementById('fb1').style.display = 'inline';
+        document.getElementById('insta1').style.display = 'inline';
+    }
+}
