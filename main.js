b15d0dff71540ede276e509f363ed4962e3e5971
Dean Simonelli <simonel3@msu.edu>
2016-12-13 10:46:54 -0500

    changes

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ function size() {
         document.getElementById('twit2').style.display = 'none';
         document.getElementById('insta2').style.display = 'none';
         document.getElementById('fb2').style.display = 'none';
-    } else {
-        document.getElementById('spartyhead1').style.display = 'inline';
-        document.getElementById('spartyhead2').style.display = 'inline';
         document.getElementById('twit1').style.display = 'inline';
         document.getElementById('fb1').style.display = 'inline';
         document.getElementById('insta1').style.display = 'inline';
+    } else {
+        document.getElementById('spartyhead1').style.display = 'inline';
+        document.getElementById('spartyhead2').style.display = 'inline';
+        document.getElementById('twit2').style.display = 'inline';
+        document.getElementById('fb2').style.display = 'inline';
+        document.getElementById('insta2').style.display = 'inline';
+        document.getElementById('twit1').style.display = 'none';
+        document.getElementById('insta1').style.display = 'none';
+        document.getElementById('fb1').style.display = 'none';
     }
 }
